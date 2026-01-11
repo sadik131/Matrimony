558fa6d0a3647f8c8c564bb050195c88779889c1
supto <salatussadik131@gmail.com>
2026-01-11 19:44:13 +0600

    solve icon problem

--- resources/views/components/social-share.blade.php
+++ resources/views/components/social-share.blade.php
@@ -25,6 +25,11 @@
         href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}"
         target="_blank"
         class="mb-2 flex rounded bg-[#3b5998] px-6 py-2.5 text-xs font-medium uppercase text-white shadow-md hover:shadow-lg transition">
+         <span class='me-2 [&>svg]:h-4 [&>svg]:w-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 320 512" class="h-4 w-4">
+        <path d="M279.14 288l14.22-92.66h-88.91V117.78c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/>
+    </svg>
+         </span>
         Facebook
     </a>
 
@@ -33,6 +38,11 @@
         href="https://www.linkedin.com/shareArticle?url={{ urlencode($url) }}&title={{ urlencode($title) }}"
         target="_blank"
         class="mb-2 flex rounded bg-[#0077b5] px-6 py-2.5 text-xs font-medium uppercase text-white shadow-md hover:shadow-lg transition">
+        <span class='me-2 [&>svg]:h-4 [&>svg]:w-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512" class="h-4 w-4">
+            <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
+            </svg>
+         </span>
         LinkedIn
     </a>
 
@@ -41,6 +51,11 @@
         href="https://wa.me/?text={{ urlencode($title.' '.$url) }}"
         target="_blank"
         class="mb-2 flex rounded bg-[#25D366] px-6 py-2.5 text-xs font-medium uppercase text-white shadow-md hover:shadow-lg transition">
+        <span class='me-2 [&>svg]:h-4 [&>svg]:w-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 448 512" class="h-4 w-4">
+                <path d="M380.9 97.1C339-6.3 207.2-35.1 117.6 54.5 40.6 131.5 30.5 249.5 91.4 337L64 448l113.2-29.6c84.6 46.4 193.4 15.2 240.5-70.7 34.3-62.3 36.1-140.5-36.8-250.6z"/>
+            </svg>
+         </span>
         WhatsApp
     </a>
 
